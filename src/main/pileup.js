257567d761bd7165299fa5014ac13ca07e619d9c
justinzh1 <justinzhongvs@gmail.com>
2016-06-28 13:18:17 -0700

    added featuredatasource to pileup.js file

--- src/main/pileup.js
+++ src/main/pileup.js
@@ -20,7 +20,7 @@ import GenotypeDataSource from './sources/GenotypeDataSource';
 import BamDataSource from './sources/BamDataSource';
 import GA4GHDataSource from './sources/GA4GHDataSource';
 import EmptySource from './sources/EmptySource';
-// import FeatureDataSource from './sources/FeatureDataSource';
+import FeatureDataSource from './sources/FeatureDataSource';
 
 // Visualizations
 import CoverageTrack from './viz/CoverageTrack';
